b71a63a8930dc4572440fc960c37f9e09532b086
maxxxxxxy <cvb.cvb.2017.cvb@gmail.com>
2023-02-27 22:57:31 +0300

    orig-link-return-func

--- src/main/java/com/springrest/linkcut/Service/LinkService.java
+++ src/main/java/com/springrest/linkcut/Service/LinkService.java
@@ -3,5 +3,5 @@ package com.springrest.linkcut.Service;
 public interface LinkService {
     String createCutLink(Long longLink);
     String getOriginalLink(String shortLink);
-    boolean isContains(char[]fullLink,char[]allowedChars);
+
 }

22933e2113f476e45da53554f6b490253459e5bc
maxxxxxxy <cvb.cvb.2017.cvb@gmail.com>
2023-02-28 21:25:12 +0300

    redirect-to-origlink

--- src/main/java/com/springrest/linkcut/Service/Impl/LinkServiceImpl.java
+++ src/main/java/com/springrest/linkcut/Service/Impl/LinkServiceImpl.java
@@ -5,40 +5,37 @@ import com.springrest.linkcut.models.UserLink;
 import com.springrest.linkcut.models.repository.UserLinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import static java.lang.Character.*;
 
 
 @Service
 public class LinkServiceImpl implements LinkService {
     @Autowired
     private UserLinkRepository userLinkRepository;
-    private UserLink userLink;
     private static final String allowedString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final char[] base62 = allowedString.toCharArray();
     private final int alphabetLen = base62.length;
+
     @Override
-    public String createCutLink(Long longLink){
+    public String createCutLink(Long longLink) {
         var resultBuild = new StringBuilder();
-        while(longLink>0) {
+        while (longLink > 0) {
             String line = String.valueOf(longLink % alphabetLen);
             if (Integer.valueOf(line) <= alphabetLen) {
                 resultBuild.append(base62[Integer.valueOf(line)]);
                 longLink /= alphabetLen;
             }
-            if(resultBuild.length()>9) break;
+            if (resultBuild.length() > 9) break;
         }
         return resultBuild.toString();
     }
+
     @Override
     public String getOriginalLink(String shortLink) {
         List<UserLink> users = userLinkRepository.UserWithExistLink(shortLink);
-        for(UserLink u:users){
-            if(!users.isEmpty()&& users.size()==1) return u.getLongLink().toString();
+        for (UserLink u : users) {
+            if (!users.isEmpty() && users.size() == 1) return u.getLongLink().toString();
             else return "Nothing to return, no exist links";
         }
         return null;

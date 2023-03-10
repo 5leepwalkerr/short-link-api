6f5ad333c823b18fa569740a14cc2b0eadb4877f
maxxxxxxy <cvb.cvb.2017.cvb@gmail.com>
2023-03-10 23:20:37 +0300

    fix-redirect

--- src/main/java/com/springrest/linkcut/Service/Impl/LinkServiceImpl.java
+++ src/main/java/com/springrest/linkcut/Service/Impl/LinkServiceImpl.java
@@ -17,21 +17,18 @@ public class LinkServiceImpl implements LinkService {
     private static final String allowedString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final char[] base62 = allowedString.toCharArray();
     private final int alphabetLen = base62.length;
+    private final String mineDomain = "http://localhost:8081/link/";
 
     @Override
     public String createCutLink(String longLink) {
         var resultBuild = new StringBuilder();
         Pattern patternLink = Pattern.compile("(?:http(?:s)?:\\/\\/)?(?:www\\.)?(?:youtu\\.be\\/|youtube\\.com\\/(?:(?:watch)?\\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\\/))([^\\?&\\\"'<> #].+)");
         Matcher matcherLink = patternLink.matcher(longLink);
-        Pattern domainPattern = Pattern.compile("(?:http(?:s)?:\\/\\/)?(?:www\\.)?(?:youtu\\.be\\/|youtube\\.com\\/(?:(?:watch)?\\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)))");
-        Matcher matcherDomain = domainPattern.matcher(longLink);
 
         String videoCode = "";
-        String domain = "";
+        resultBuild.append(mineDomain);
 
         if(matcherLink.find()) videoCode = matcherLink.group(1);
-        if(matcherDomain.find()) domain = matcherDomain.group(0);
-        resultBuild.append(domain);
 
         String regexOnlyNums = "//d+"; // only digits 0-9
         if(videoCode.matches(regexOnlyNums)) {
@@ -53,14 +50,14 @@ public class LinkServiceImpl implements LinkService {
                    char code = base62[charPosition];
                    resultBuild.append(code);
                }
-               if(resultBuild.length()-domain.length() >9)break;
+               if(resultBuild.length()-mineDomain.length() >8)break;
             }
         }
         return resultBuild.toString();
     }
     @Override
     public String getOriginalLink(String shortLink) {
-        List<UserLink> users = userLinkRepository.UserWithExistLink(shortLink);
+        List<UserLink> users = userLinkRepository.UserWithExistLink(mineDomain+shortLink);
         for (UserLink u : users) {
             if (!users.isEmpty() && users.size() == 1) return u.getLongLink().toString();
             else return "Nothing to return, no exist links";

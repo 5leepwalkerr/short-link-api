08e74fa309b0ee10646ff5770f5e0b389641546e
maxxxxxxy <cvb.cvb.2017.cvb@gmail.com>
2023-03-16 01:40:33 +0300

    simple-html-page

--- src/main/java/com/springrest/linkcut/Controller/DefaultController.java
+++ src/main/java/com/springrest/linkcut/Controller/DefaultController.java
@@ -1,18 +1,14 @@
 package com.springrest.linkcut.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "test";
+    @RequestMapping("/link")
+    public String html() {
+        return "index.html";
     }
 }
